ff7eae2c7ab359c2506b4ac6a613b4ecdc7b659b
Jermaine Marshall <jermainemarshall@jermaines-mbp.dhcp.nd.edu>
2019-04-22 12:52:28 -0400

    modified based on template

--- jscript/tabs_color_script.js
+++ jscript/tabs_color_script.js
@@ -1,32 +1,39 @@
 <script>
-    
-    $('div#colorSelection').on('click', 'div', function(event){
-	    $('div.selectedColor').removeClass('selectedColor');
-	    $(this).addClass('selectedColor');
-	})
-    $('div#squareContainer').on('click', 'div', function(event){
-	    var color = $('div.selectedColor').css('background-color');
-	    var squareId = $(this).id;
+$('div#colorSelection').on('click', 'div', function(event){
+    $('div.selectedColor').removeClass('selectedColor');
+    $(this).addClass('selectedColor');
+})
+$('div#Paper-Tabs').on('click', 'div', function(event){
+    var color = $('div.selectedColor').css('background-color');
+    var squareId = $(this).id;
 
-	    $(this).css({"background-color":color});
-	    $('input[name="'+squareId+'"]').val(color);
+    $(this).css({"background-color":color});
+    $('input[name="'+squareId+'"]').val(color);
 
-	});
-
-
-
-    function opentab(evt, topic) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-	tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-	tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(topic).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+});
 </script>
-   
+    
+
+
+<script>
+function convertDate(d) {
+  var p = d.split("/");
+  return +(p[2]+p[1]+p[0]);
+}
+
+function sortByDate() {
+  var tbody = document.querySelector("#results tbody");
+  // get trs as array for ease of use
+  var rows = [].slice.call(tbody.querySelectorAll("tr"));
+  
+  rows.sort(function(a,b) {
+    return convertDate(a.cells[0].innerHTML) - convertDate(b.cells[0].innerHTML);
+  });
+  
+  rows.forEach(function(v) {
+    tbody.appendChild(v); // note that .appendChild() *moves* elements
+  });
+}
+
+
+    </script>  
