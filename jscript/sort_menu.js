87adfcbd9d6e1d0f5923dc52bab7f9e92d468ce6
Trenton L. Ford <tford5@nd.edu>
2019-04-24 12:32:02 -0400

    added hook for tupled dataset, and basic sorting functionality

--- jscript/sort_menu.js
+++ jscript/sort_menu.js
@@ -3,11 +3,19 @@ var svgContainer = d3.select("#sort_space").append("svg")
     .attr("height", "100%")
     .attr("viewBox", "0 0 360 480");
 
-function cardFactory(data = [0,1,2], x = 0, y = 0, textSelection = "Sample Text") {
+function cardFactory(data = [['red', 'sample text1', new Date(2014, 1, 1)], ['blue', 'sample text2', new Date(2013, 1, 1)]], x = 0, y = 0) {
     /* Should Return a card object that can then be appended to
     *  the svg at a certain point */
-    var date = new Date();
+    // var date = new Date();
     // alert("cardFactory")
+
+    function date_sort(a, b) {
+        if (a[3] < b[3]) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 
 
 // Card Shape variables
@@ -17,11 +25,11 @@ function cardFactory(data = [0,1,2], x = 0, y = 0, textSelection = "Sample Text"
 
     // create card element
     var card = svgContainer.selectAll("g")
-        .data(data)
+        .data(data.sort(date_sort))
         .enter().append("g")
         .attr('pointer-events', 'all')
         .attr("transform", function (d, i) {
-            return "translate(" + 5 + "," + (d * 125 + 50) + ")";
+            return "translate(" + 5 + "," + (i * 125 + 50) + ")";
         });
     // add border to card
     card.append('rect')
@@ -37,12 +45,13 @@ function cardFactory(data = [0,1,2], x = 0, y = 0, textSelection = "Sample Text"
 
     // Add currently selected text to card
     card.append("text")
+    // .data(data)
         .attr("y", y + 55)
         .attr("x", x + 120)
         .attr("dy", ".35em")
-        .text(function (d) {
+        .text(function (d, i) {
             // return document.getSelection().toString();
-            return "test text" + d;
+            return d[1];
         });
 
     // Add TimeStamp to card
@@ -50,17 +59,20 @@ function cardFactory(data = [0,1,2], x = 0, y = 0, textSelection = "Sample Text"
         .attr("y", y + 110)
         .attr("x", x + 225)
         .attr("dy", ".35em")
-        .text(function () {
-            return date.toDateString();
+        .text(function (d) {
+            return d[2].toDateString();
         });
 
     // Add Selected color rectangle to card
     card.append('rect')
+    // .data(data)
         .attr('x', x + 5)
         .attr('y', y + 5)
         .attr('width', 20)
         .attr('height', 20)
-        .attr('fill', 'red')
+        .attr('fill', function (d) {
+            return d[0];
+        })
         .attr('stroke', borderColor)
         .attr('border', borderWidth);
 }
@@ -82,8 +94,8 @@ function handleMouseOver(d, i) {  // Add interactivity
             return 15;
         }
     })
-        // .text(function () {
-        //     return document.getSelection().toString();});
+    // .text(function () {
+    //     return document.getSelection().toString();});
 }
 
 function handleMouseOut(d, i) {
