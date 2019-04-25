6b50f8ba1c717603b5b3a1951e3ee9cd47c1a8be
Trenton L. Ford <tford5@nd.edu>
2019-04-24 22:45:22 -0400

    add scroll bar to sort window

--- jscript/sort_menu.js
+++ jscript/sort_menu.js
@@ -1,19 +1,19 @@
 var svgContainer = d3.select("#sort_space").append("svg")
     .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("viewBox", "0 0 360 480");
+    .attr("height", "95%")
+    // .attr("viewBox", "0 0 360 ");
 
 var div = d3.select("#sort_space").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-var local_data = [['red', 'sample text1', new Date(2014, 1, 1)], ['blue', 'sample text2', new Date(2013, 1, 1)]];
+// var local_data = [['red', 'sample text1', new Date(2014, 1, 1)], ['blue', 'sample text2', new Date(2013, 1, 1)]];
 
 function cardFactory(data = annotation_array, x = 0, y = 0) {
     /* Should Return a card object that can then be appended to
     *  the svg at a certain point */
 
-    // alert("cardFactory")
+    svgContainer.attr('height', (annotation_array.length*125 + 50));
 
     function date_sort(a, b) {
         if (a[3] < b[3]) {
@@ -23,11 +23,10 @@ function cardFactory(data = annotation_array, x = 0, y = 0) {
         }
     }
 
-
 // Card Shape variables
     var borderColor = 'black';
     var borderWidth = 10;
-    var cardDims = [350, 120]
+    var cardDims = [500, 120]
 
     // create card element
     var card = svgContainer.selectAll("g")
@@ -55,17 +54,25 @@ function cardFactory(data = annotation_array, x = 0, y = 0) {
     // .data(data)
         .attr("y", y + 50)
         .attr("x", x + 30)
-        .call(wrap, cardDims[0] - 50)
+        .text(function (d) {
+            var cutoff = 200;
+            if (d[1].toString().length > cutoff) {
+                return (d[1].toString().substring(0, cutoff) + "...");
+            } else {
+                return d[1].toString();
+            }
+        })
+        .call(wrap, 480);
 
     // Add TimeStamp to card
     card.append("text")
         .attr("y", y + 110)
-        .attr("x", x + 225)
+        .attr("x", x + 400)
         .attr("dy", ".35em")
-        .attr("fontSize", "small")
+        .attr("fontSize", "xx-small")
         .text(function (d) {
             var date_str = d[2].getHours() + ":" + d[2].getMinutes() + "  " + d[2].getMonth() + "/" + d[2].getDay() + "/" + d[2].getFullYear();
-            return d[2].toTimeString();
+            return date_str;
         });
 
     // Add Selected color rectangle to card
@@ -88,8 +95,7 @@ function handleMouseOver(d, i) {  // Add interactivity
     d3.select(this).attrs({
         fill: "gainsboro",
     });
-    var formatTime = d3.time.format("%e %B");
-
+    // var formatTime = d3.time.format("%e %B");
 
     // Specify where to put label of text
     svgContainer.append("text").attrs({
@@ -101,13 +107,6 @@ function handleMouseOver(d, i) {  // Add interactivity
             return 15;
         }
     });
-    div.transition()
-        .duration(200)
-        .style("opacity", .9);
-    div.html(formatTime(d[2]) + "<br/>")
-        .style("left", (0) + "px")
-        .style("top", (i*125) + "px");
-
 }
 
 function handleMouseOut(d, i) {
@@ -115,7 +114,6 @@ function handleMouseOut(d, i) {
     d3.select(this).attrs({
         fill: "none",
     });
-
     // Select text by id and then remove
     d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
 
@@ -125,16 +123,21 @@ function handleMouseOut(d, i) {
 }
 
 function wrap(text, width) {
-    text.each(function() {
+    text.each(function () {
         var text = d3.select(this),
             words = text.text().split(/\s+/).reverse(),
             word,
             line = [],
             lineNumber = 0,
             lineHeight = 1.1, // ems
+            x = text.attr("x"),
             y = text.attr("y"),
-            dy = parseFloat(text.attr("dy")),
-            tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+            dy = 0, //parseFloat(text.attr("dy")),
+            tspan = text.text(null)
+                .append("tspan")
+                .attr("x", x)
+                .attr("y", y)
+                .attr("dy", dy + "em");
         while (word = words.pop()) {
             line.push(word);
             tspan.text(line.join(" "));
@@ -142,13 +145,12 @@ function wrap(text, width) {
                 line.pop();
                 tspan.text(line.join(" "));
                 line = [word];
-                tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                tspan = text.append("tspan")
+                    .attr("x", x)
+                    .attr("y", y)
+                    .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                    .text(word);
             }
         }
     });
-}
-
-function type(d) {
-    d.value = +d.value;
-    return d;
 }
